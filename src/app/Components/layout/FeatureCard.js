8994ab8e9fe247aba6d7edc64359f8b835cb4305
Riddhi Kulkarni <riddhikul26@gmail.com>
2024-04-01 18:18:22 +0530

    feature card

--- src/app/Components/layout/FeatureCard.js
+++ src/app/Components/layout/FeatureCard.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
-function FeatureCard({ title, description, buttonText }) {
+function FeatureCard({ title, description, buttonText, photoSrc }) {
   return (
     <div className="max-w-md rounded overflow-hidden shadow-lg bg-gradient-to-br from-neonPink to-neonBlue p-6">
-      <div className="text-white font-bold text-xl mb-2">{title}</div>
-      <p className="text-white text-base">{description}</p>
+      {photoSrc && <img className="mb-1 h-56 w-64 ml-12 object-center " src={photoSrc} alt="Feature" />}
+      <div className="text-white font-bold text-xl mb-2 text-center">{title}</div>
+
+      {/* <p className="text-white text-base">{description}</p> */}
       {buttonText && (
-        <button className="mt-4 bg-white text-neonPink font-semibold py-2 px-4 rounded-lg border border-neonPink hover:bg-transparent hover:text-white transition duration-300">
+        <button className=" bg-white text-neonPink font-semibold py-1 px-4 rounded-lg border border-neonPink hover:bg-transparent hover:text-white transition duration-300">
           {buttonText}
         </button>
       )}
--- src/app/Components/layout/Features.js
+++ src/app/Components/layout/Features.js
@@ -1,34 +1,39 @@
-import React from 'react';
-import FeatureCard from './FeatureCard';
+import React from "react";
+import FeatureCard from "./FeatureCard";
 
 function Features() {
-  return (<>
-        <div className='text-white text-4xl text-center font-bold mb-10'>Our Features</div>
-
-    <div className="flex justify-center gap-8">
-        
-      <div className="flex">
-        <FeatureCard
-          title="Feature 1"
-          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
-          buttonText="Learn More"
-        />
+  return (
+    <>
+      <div className="text-white text-4xl text-center font-bold mb-10">
+        Our Features
       </div>
-      <div className="flex">
-        <FeatureCard
-          title="Feature 2"
-          description="Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-          buttonText="Explore"
-        />
-      </div>
-      <div className="flex">
-        <FeatureCard
-          title="Feature 3"
-          description="Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
-          buttonText="Try Now"
-        />
-      </div>
-    </div> </>
+      <div className="flex justify-center gap-8">
+        <div className="flex-1">
+          <FeatureCard
+            title="Analysis and Reporting"
+            description="Feature description goes here."
+            buttonText="Learn More"
+            photoSrc="./f1.png"
+          />
+        </div>
+        <div className="flex-1">
+          <FeatureCard
+            title="User Friendly"
+            description="Feature description goes here."
+            buttonText="Learn More"
+            photoSrc="./f2.png"
+          />
+        </div>
+        <div className="flex-1">
+          <FeatureCard
+            title="Suitable for all Organizations"
+            description="Feature description goes here."
+            buttonText="Learn More"
+            photoSrc="./f3.png"
+          />
+        </div>
+      </div>{" "}
+    </>
   );
 }
 

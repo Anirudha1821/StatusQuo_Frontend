89d87ad1aaec9f10efa61a99e2da70aa3a8c3c2a
Anirudha0721 <anirudha@rhythmflows.com>
2024-04-05 01:10:00 +0530

    Merge:Rdev2_1 fixed conflict

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -3,11 +3,7 @@ import Card from "../Components/dashboard/card/card";
 import Chart from "../Components/dashboard/chart/chart";
 import styles from "../Components/dashboard/dashboard.module.css";
 import Rightbar from "../Components/dashboard/rightbar/rightbar";
-<<<<<<< HEAD
 import Tasks from "../Components/dashboard/tasks/tasks";
-=======
-import Transactions from "../Components/dashboard/tasks/transactions";
->>>>>>> origin/Rdev2.0
 
 const Dashboard = () => {
   return (

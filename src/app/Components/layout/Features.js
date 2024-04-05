160996c40dc2ff70b2308a32269dcb338c63aa05
Riddhi Kulkarni <riddhikul26@gmail.com>
2024-04-05 09:32:42 +0530

    feature alignment fix

--- src/app/Components/layout/Features.js
+++ src/app/Components/layout/Features.js
@@ -3,12 +3,12 @@ import FeatureCard from "./FeatureCard";
 
 function Features() {
   return (
-    <div id="features">
+    <div id="features" className="mb-10">
       <div className="text-white text-4xl text-center font-bold mb-10">
         Our Features
       </div>
       <div className="flex justify-center gap-8">
-        <div className="flex-1">
+        <div className="flex-1 ml-10">
           <FeatureCard
             title="Analysis and Reporting"
             description="Feature description goes here."
@@ -16,7 +16,7 @@ function Features() {
             photoSrc="./f1.png"
           />
         </div>
-        <div className="flex-1">
+        <div className="flex-1 mx-auto">
           <FeatureCard
             title="User Friendly"
             description="Feature description goes here."
@@ -24,7 +24,7 @@ function Features() {
             photoSrc="./f2.png"
           />
         </div>
-        <div className="flex-1">
+        <div className="flex-1 mx-auto">
           <FeatureCard
             title="Suitable for all Organizations"
             description="Feature description goes here."
